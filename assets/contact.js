89de1e195b663b0fafa8c7c17c299b2659ca4c81
Lise <lise692@gmail.com>
2022-08-21 03:27:06 +0200

    add contact page and dark mode

--- assets/contact.js
+++ assets/contact.js
@@ -1,8 +1,3 @@
-// window.addEventListener("scroll", (e) => {
-//     console.log(this.scrollY);
-//     console.log(this.scrollX);
-// });
-
 // => MENU
 let buttonMenu = document.querySelector('.open__menu');
 let header = document.querySelector('header');
@@ -22,56 +17,6 @@ buttonMenu.addEventListener('click', () => {
 
     buttonMenu.classList.toggle('fa-xmark');
     buttonMenu.classList.toggle('fa-bars');    
-});
-
-//  => SLIDESHOW
-let buttonPrevious = document.querySelector('.button__previous');
-let buttonNext = document.querySelector('.button__next');
-let listSlide = document.querySelector('slide__projects');
-let slides = document.querySelectorAll('.slide');
-let nbSlide = slides.length;
-
-buttonPrevious.addEventListener('click', slideprev)
-buttonNext.addEventListener('click', slideNext)
-
-function slideprev() {
-    var currentSlide = document.querySelector('.show__slide');
-    var prevslide = currentSlide.previousElementSibling;
-
-    if (prevslide == null) {
-        prevslide = slides[nbSlide - 1];
-    }
-    currentSlide.classList.remove('show__slide');
-    prevslide.classList.add('show__slide');
-}
-
-function slideNext() {
-    var currentSlide = document.querySelector('.show__slide');
-    var nextslide = currentSlide.nextElementSibling;
-    
-    if (nextslide == null) {
-        nextslide = slides[0];
-    }
-    currentSlide.classList.remove('show__slide');
-    nextslide.classList.add('show__slide');
-}
-
-// => SHOW POP UP SKILL
-let skills = document.querySelectorAll('.skill');
-let popUp = document.querySelectorAll('.pop__up__skill');
-
-skills.forEach(element => {
-    let hisPopUp = element.firstElementChild;
-
-    element.addEventListener('mouseover', () => {
-        hisPopUp.style.opacity = '1';
-    })
-
-    element.addEventListener('mouseout', ()  => {
-        return setTimeout( () => {
-            return hisPopUp.style.opacity = '0';
-        }, 500)
-    })
 });
 
 // => TOGGLE DARK / LIGHT MODE
